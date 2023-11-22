aaaa6820037bd9a9a5c829bc8d67e79b8ad5b210
AksanaRaketskaya <33267307+AksanaRaketskaya@users.noreply.github.com>
2023-11-22 17:49:38 +0300

    task 3.1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,15 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(path) {
+  const fields = path.split('.');
+  return obj => {
+    let value = obj[fields.shift()];
+    for (let field of fields) {
+      if (!value) break;
+      value = value?.[field];
+    }
+    return value;
+  }
 }
